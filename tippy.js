d150726183fd0342a839dcddf7641cdf5ada6607
Amir <amirnazari2003@gmail.com>
2024-09-25 15:30:31 +0330

    personal site

---
+++ tippy.js
@@ -0,0 +1,38 @@
+tippy('#aw', {
+    content: "خانه",
+    arrow: false,
+    
+    
+  });
+  tippy('#user', {
+    content: "درباره",
+    arrow: false,
+    
+    
+  });
+  tippy('#book', {
+    content: "رزومه",
+    arrow: false,
+    
+    
+  });
+  tippy('#shop', {
+    content: "نمونه کارها",
+    arrow: false,
+    
+    
+  });
+
+  tippy('#mes', {
+    content: "تماس",
+    arrow: false,
+    
+    
+  });
+
+  tippy('#blog', {
+    content: "بلاگ",
+    arrow: false,
+    
+    
+  });
---
+++ xc.js
@@ -0,0 +1,10 @@
+const swiperSlider=new Swiper('.swiper',{
+    loop:true,
+    spaceBetween:5,
+    slidesPerView: 2,
+    speed: 400,
+    enabled:true,
+    autoplay: {
+        delay: 2000,
+      },
+  })
